1ae086233cba88817b6bf55ef56ff5bbf727b710
Jr-Studios001 <jrorlando109197@gmail.com>
2025-06-12 21:24:26 -0500

    version 03

--- app/vistas/Modulos/main.php
+++ app/vistas/Modulos/main.php
@@ -4,14 +4,31 @@
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.html">Inicio</a></li>
-          <li class="breadcrumb-item active">Panel de control</li>
+          <li class="breadcrumb-item active">Panel de control.</li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
 
     <section class="section dashboard">
       <div class="row">
-
+        <div class="" >
+          <div class="pagetitle">
+            <div class="breadcrumb">
+              <div class="breadcrumb-item active">
+                <?php
+                echo"<h1>bienvenido de nuevo "  . $_SESSION["usuario"]["nombre"] ."</h1>";
+                //if (isset($_SESSION["usuario"])) {
+                  //echo"bienvenido de nuevo "  . $_SESSION["usuario"]["nombre"];
+                //}
+                ?>
+              </div>
+              
+            </div>
+            
+          </div>
+          
+        </div>
+        
       </div>
     </section>
 
